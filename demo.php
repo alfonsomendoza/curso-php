2213a877f872f9b3f75de9b6456f9149b711341a
alfonsomendoza <ALFONSOJOSE1@GMAIL.COM>
2024-05-09 10:32:34 -0600

    Mensaje del commit

---
+++ demo.php
@@ -0,0 +1,35 @@
+<h1><?php
+    echo "hola mundo";
+    $a = 6;
+    $b = 6;
+    $suma = $a + $b;
+    echo "$suma";
+    $age=39;
+    $new_age=$age+"1";
+
+      echo "$new_age"
+
+    ?>
+</h1>
+<style>
+    :root {
+        color-scheme: Light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
+
+<P><?php 
+  echo $new_age;
+?></P>
+
+<h2><?=
+    $new_age;
+?></h2>
+ <script>alert("Hola mundo")</script>
+
+
+ <?php phpinfo() ?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+const API_URL = "https://whenisthenextmcufilm.com/api";
+#Iniccializar una sesion de curl;ch=curl handle
+$ch = curl_init(API_URL);
+//Indicar que queremos recibit el resultado de la peticion y no mostrarla en pantalla
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/*Ejecutar la peticion
+y guardamos el resultado
+ */
+
+// una alternativa seria ultizar file_get_contents
+// $result=file_get_contents(API_URL);// si solo quieres hacer un GET de una API
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+
+curl_close($ch);
+
+?>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>La proxima pelicula de Marvel</title>
+    <meta name="description" content="La Proxima Pelicula de Marvel" />
+    <meta name="viemport" content="width=device-width,initial-scale-1.0" />
+    <!-- Centered viewport -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
+</head>
+
+<main>
+   <!-- <pre style="font-size:10px; overflow: scroll; height:250px;">
+      //  <?php
+        // var_dump($data);
+        ?>//
+        </pre>   -->
+        <section>
+          <img src="<?= $data["poster_url"]; ?>" width="350" alt="Poster de <?= $title ?>" style="border-radius: 16px"  />
+
+            <!--  <h2>La Proxima Pelicula de Marvel</h2>-->
+        </section>
+        <hgroup>
+            <h3><?= $data["title"]; ?>se estrena en <?= $data["days_until"]; ?></h3>
+            <p>Fecha de estreno:<?= $data["release_date"]; ?></p>
+            <p>La siguiente es:<?= $data["following_production"]["title"]; ?></p>
+        </hgroup>
+
+</main>
+
+
+
+
+<style>
+    :root {
+        color-scheme: Light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+    section{
+        display:flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup{
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
